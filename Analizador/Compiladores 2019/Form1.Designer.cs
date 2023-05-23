e2337344816c6fe38944b75cc73967f5b99af980
Gulliver626 <gerry626.2000@gmail.com>
2023-05-23 16:27:00 -0500

    first commit

---
+++ Analizador/Compiladores 2019/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Compiladores_2019
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.c1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeight = 20;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.c1,
+            this.c2,
+            this.c3,
+            this.c4});
+            this.dataGridView1.Location = new System.Drawing.Point(595, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 15;
+            this.dataGridView1.Size = new System.Drawing.Size(451, 540);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // c1
+            // 
+            this.c1.HeaderText = "LEXEMA";
+            this.c1.MinimumWidth = 50;
+            this.c1.Name = "c1";
+            this.c1.ReadOnly = true;
+            this.c1.Width = 175;
+            // 
+            // c2
+            // 
+            this.c2.HeaderText = "TOKEN";
+            this.c2.MinimumWidth = 50;
+            this.c2.Name = "c2";
+            this.c2.ReadOnly = true;
+            // 
+            // c3
+            // 
+            this.c3.HeaderText = "LINEA";
+            this.c3.MinimumWidth = 50;
+            this.c3.Name = "c3";
+            this.c3.ReadOnly = true;
+            this.c3.Width = 80;
+            // 
+            // c4
+            // 
+            this.c4.HeaderText = "COLUMNA";
+            this.c4.MinimumWidth = 65;
+            this.c4.Name = "c4";
+            this.c4.ReadOnly = true;
+            this.c4.Width = 80;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(595, 580);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(451, 66);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "LEXICO";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(707, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ANALIZADOR LEXICO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(237, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "CODIGO";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBox1.Font = new System.Drawing.Font("Lucida Bright", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(16, 34);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(557, 360);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(16, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(557, 66);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "ARCHIVO";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1064, 658);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "DOOES";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c4;
+    }
+}
+
---
+++ Analizador/Compiladores 2019/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Compiladores_2019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                        
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            
+           dataGridView1.Rows.Clear();
+                  
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+                        this.Analizar();
+
+                      
+        }
+
+
+
+        string unir_cad = "";
+        string espacio = "[' ']";
+        string salto = "['\n']";
+     
+        string unir_string = "";
+        string unir_com = "";
+
+        int contar_columas = 1;
+        int contar_lineas = 1;
+        
+        public void insertartexto()
+        {
+            openFileDialog1.Filter = "TextFile | *.txt";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string spath = openFileDialog1.FileName;
+                StreamReader SR = new StreamReader(spath);
+                string data = SR.ReadToEnd();
+                textBox1.Text = data;
+
+            }
+        }
+
+        public void Analizar()
+        {
+            dataGridView1.Rows.Clear();
+
+            contar_columas = 1;
+            contar_lineas = 1;
+
+
+            char validar_com = '0';
+            char validar_cad_string = '0';
+
+            string comentario = "[//]";
+            string cad_string = "[”|”]";
+            string texto = textBox1.Text;
+
+
+
+            foreach (char letra in texto)
+            {
+                string letra2 = letra.ToString();           
+
+
+                if (Regex.IsMatch(letra2, cad_string))
+                {
+                    if (validar_cad_string.Equals('0'))
+                    {
+                        validar_cad_string = '1';
+                    }
+                    else
+                    {
+                        
+                        dataGridView1.Rows.Add(unir_string + "”", "mensaje", contar_lineas, contar_columas);
+                        //contar_lineas += 1;
+                        validar_cad_string = '0';
+                        unir_string = "";
+
+
+                    }
+                }
+
+                if (validar_cad_string.Equals('1'))
+                {
+                    unir_string = unir_string + letra2;
+                }
+
+
+                if (Regex.IsMatch(letra2, comentario))
+                {
+                    validar_com = '1';
+                }
+
+                if (validar_com.Equals('1'))
+                {
+                    unir_com = unir_com + letra2;
+
+                    if (letra.Equals('\n'))
+                    {
+
+                        dataGridView1.Rows.Add(unir_com + "", "comentario", contar_lineas, contar_columas);
+                        contar_lineas += 1;
+                        contar_columas = 1;
+                        validar_com = '0';
+                        unir_com = "";
+
+
+                    }
+
+                }
+
+                else if (validar_com.Equals('0') & validar_cad_string.Equals('0') & letra2 != "\"" & letra2 != "\r")
+                {
+                    if (letra2 == " " || letra2=="\n")
+                    {
+                        this.AnalizarPalabras();
+                       
+                        if (Regex.IsMatch(letra2, espacio))
+                        {
+                            contar_columas += 1;
+                        }
+                        if (Regex.IsMatch(letra2, salto))
+                        {
+                            contar_lineas += 1;
+                            contar_columas = 1;
+                        }
+                    }
+                    else
+                    {
+                        unir_cad = unir_cad + letra2;
+                    }
+                    
+                }
+
+
+            } //fin foreach
+
+        }
+
+
+        public void AnalizarPalabras()
+        {
+            this.VerificarLexema();          
+        }
+
+
+        char validar_uno_mas = '0';
+        public void VerificarLexema()
+        {
+
+
+            string[] reservado = {  "main", "include", "std", "cout", "cin", "endl", "return", "iostream.h", "conio.h", 
+                    "assert.h", "stdio.h", "if","else","while","for","namespace" };
+            string[] identificador = { "suma","resta","multiplicacion","division","resultado"};
+            string[] operador = { ">>", "<<", "::", "=", "-"};
+            string[] tipodato = { "int", "float", "char", "short", "long", "double", "bool","void","string" };
+            string exp_numeros = "^[0-9]+$[0-9]?";
+            string exp_delimitador = "^[;|(|)|{|}|”]$";
+            string exp_operadores = "^[+|/|*]$";
+            string asignacion = "^#$";
+            string exp_comparador = "^[<|>]$|^==$";
+            string variable = "^numero[(A-Z)?]$";
+            string ident = "^[a-z]$";                   
+           
+
+            char validar_reservado = '0';
+
+
+
+            for (int i = 0; i < 16; i++)
+            {
+                if (unir_cad.Equals(reservado[i]))
+                {
+                    dataGridView1.Rows.Add(unir_cad + "", "reservada", contar_lineas, contar_columas);
+                    validar_reservado = '1';
+                    if (Regex.IsMatch(unir_cad, "si"))
+                    {
+                        validar_uno_mas = '1';
+                    }
+                }
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                if (unir_cad.Equals(identificador[i]))
+                {
+                    dataGridView1.Rows.Add(unir_cad + "", "variable", contar_lineas, contar_columas);
+                    validar_reservado = '1';
+                    if (Regex.IsMatch(unir_cad, "si"))
+                    {
+                        validar_uno_mas = '1';
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (unir_cad.Equals(operador[i]))
+                {
+                    dataGridView1.Rows.Add(unir_cad + "", "operador", contar_lineas, contar_columas);
+                    validar_reservado = '1';
+                    if (Regex.IsMatch(unir_cad, "si"))
+                    {
+                        validar_uno_mas = '1';
+                    }
+                }
+
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (unir_cad.Equals(tipodato[i]))
+                {
+                    dataGridView1.Rows.Add(unir_cad + "", "tipo de dato", contar_lineas, contar_columas);
+                    validar_reservado = '1';
+                    if (Regex.IsMatch(unir_cad, "si"))
+                    {
+                        validar_uno_mas = '1';
+                    }
+                }
+
+            }
+
+            if (Regex.IsMatch(unir_cad, exp_numeros))
+            {
+                dataGridView1.Rows.Add(unir_cad + "", "numero", contar_lineas, contar_columas);
+            }
+            else if (Regex.IsMatch(unir_cad, exp_delimitador))
+            {
+                dataGridView1.Rows.Add(unir_cad + "", "delimitador", contar_lineas, contar_columas);
+
+            }
+            else if (Regex.IsMatch(unir_cad, exp_operadores))
+            {
+                dataGridView1.Rows.Add(unir_cad + "", "operador", contar_lineas, contar_columas);
+            }
+            else if (Regex.IsMatch(unir_cad, asignacion))
+            {
+                dataGridView1.Rows.Add(unir_cad + "", "asignacion", contar_lineas, contar_columas);
+            }
+            else if (Regex.IsMatch(unir_cad, exp_comparador))
+            {
+                dataGridView1.Rows.Add(unir_cad + "", "comparador", contar_lineas, contar_columas);
+            }
+            else if (Regex.IsMatch(unir_cad, variable))
+            {
+                dataGridView1.Rows.Add(unir_cad + "", "variable", contar_lineas, contar_columas);
+            }
+            else if (Regex.IsMatch(unir_cad, ident))
+            {
+                dataGridView1.Rows.Add(unir_cad + "", "variable", contar_lineas, contar_columas);
+            }
+
+            else if (validar_reservado.Equals('0') & unir_cad != "" & unir_cad != "\"")
+            {
+
+                dataGridView1.Rows.Add(unir_cad + "", "error de lexema", contar_lineas, contar_columas);
+
+            }
+            unir_cad = "";
+        }
+
+
+
+
+
+       private void Form1_Load(object sender, EventArgs e)
+       {
+
+       }
+      
+       private void richTextBox1_TextChanged(object sender, EventArgs e)
+       {
+
+          
+       }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.insertartexto();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
